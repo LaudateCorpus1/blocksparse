08497b50af7a567b056c647f0c2e1d6d51a4ace5
Juan Lozano <juan@kite.com>
2021-01-03 18:13:00 -0800

    wrong return type

--- src/cuda_stream.h
+++ src/cuda_stream.h
@@ -13,7 +13,7 @@ using namespace tensorflow;
 //   - https://github.com/petewarden/tensorflow_makefile/blob/master/tensorflow/stream_executor/cuda/cuda_stream.h
 //   - https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/util/gpu_kernel_helper.h#L86
 //   - and because cudaStream_t is interchangeable with CUstream (https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__DRIVER.html)
-CUstream* get_custream(OpKernelContext* ctx) {
+CUstream get_custream(OpKernelContext* ctx) {
   const CUstream* ptr = CHECK_NOTNULL(reinterpret_cast<const CUstream*>(ctx->op_device_context()->stream()->implementation()->GpuStreamMemberHack()));
   
   return *ptr;
@@ -26,7 +26,7 @@ CUstream* get_custream(OpKernelContext* ctx) {
 using namespace tensorflow;
 using perftools::gputools::cuda::CUDAStream;
 
-CUstream* get_custream(OpKernelContext* ctx) {
+CUstream get_custream(OpKernelContext* ctx) {
   return ((CUDAStream*)ctx->op_device_context()->stream()->implementation())->cuda_stream();
 }
 
